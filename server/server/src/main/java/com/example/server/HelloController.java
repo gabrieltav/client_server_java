d80edd0949b141efbe822941ea8b11b19effc696
CarlosBitizin <carlospintorossy07@gmail.com>
2022-07-07 00:30:11 -0400

    interface creation and server startup

--- server/server/src/main/java/com/example/server/HelloController.java
+++ server/server/src/main/java/com/example/server/HelloController.java
@@ -1,14 +1,84 @@
 package com.example.server;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button_send;
+    @FXML
+    private TextField tf_message;
+    @FXML
+    private VBox vbox_messages;
+    @FXML
+    private ScrollPane sp_main;
+
+    private Server server;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        try{
+            server = new Server(new ServerSocket(1234));
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Erro ao criar o servidor.");
+        }
+
+        vbox_messages.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                sp_main.setVvalue((Double) t1);
+            }
+        });
+
+        server.receiveMessageFromClient(vbox_messages);
+
+        button_send.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String menssageToSend = tf_message.getText();
+                HBox hBox = null;
+                if (!menssageToSend.isEmpty()) {
+                    hBox = new HBox();
+                    hBox.setAlignment(Pos.CENTER_RIGHT);
+                    hBox.setPadding(new Insets(5, 5, 5, 10));
+                }
+                Text text = new Text(menssageToSend);
+                TextFlow textFlow = new TextFlow(text);
+
+                textFlow.setStyle("-fx-color: rgb(239,242,255" +
+                        "-fx-background-color: rgb(15,125,242)" +
+                        "-fx-background-radius: 20px");
+
+                textFlow.setPadding(new Insets(5, 10, 5, 10));
+                text.setFill(Color.color(0.934, 0.945, 0.996));
+
+                hBox.getChildren().add(textFlow);
+                vbox_messages.getChildren().add(hBox);
+
+                server.sendMessageToCliente(menssageToSend);
+            }
+        });
     }
 }
